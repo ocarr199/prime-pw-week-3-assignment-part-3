e176833d907529111d0f36ff46cd919a69d1ba4f
Owen Carruth <ocarr199@gmail.com>
2021-04-12 16:21:02 -0500

    Got throug required portion of assignment

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -1,35 +1,46 @@
 console.log('****** Part Supply *******');
 // REQUIRED FEATURES
 
-// 1. Create a variable called 'partsNeeded' with a value of the number 40 
+// 1. Create a variable called 'partsNeeded' with a value of the number 40
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
-
+let partsNeeded = 40;
+console.log(partsNeeded);
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
-
+let supplyChanges = [3, 5, -6, 0, 7, 11]
 // 3. Console log the value of 'supplyChanges' at index 2
 console.log('3. Item at index 2 is:');
-
-// 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
+console.log(supplyChanges[2]);
+// 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
 console.log('4. Removed item:');
-
+let removedItem = supplyChanges.pop()
+console.log(removedItem);
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
-
+supplyChanges.push(25)
+console.log(supplyChanges);
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
-//    - if it is a positive number (greater than 0), log 'Added x parts.' 
+//    - if it is a positive number (greater than 0), log 'Added x parts.'
 //    - if the value is 0, skip it. Do not log it to the console.
-//    - if the value is negative, format the log as 'Part count -x.' 
-console.log('6. Showing supplyChanges...');
+//    - if the value is negative, format the log as 'Part count -x.'
+let x = supplyChanges[supplyChanges.length]
 
+console.log('6. Showing supplyChanges...');
+for(x = 0; x<=supplyChanges.length;x++){
+  if(x>0){
+    console.log("Added "+ x + " Parts")
+  }else if(x<0){
+    console.log("Part Count" + x );
+  }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
-// 7. Rewrite the `for` loop from #6 as a `for of` loop. 
+// 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
 
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
